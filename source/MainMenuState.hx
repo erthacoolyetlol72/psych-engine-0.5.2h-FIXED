c29f9fcff2fdb98674da387625fe99c151b8fb39
erthacoolyetlol72 <96339760+erthacoolyetlol72@users.noreply.github.com>
2022-11-20 20:52:38 +0100

    Update MainMenuState.hx

--- source/MainMenuState.hx
+++ source/MainMenuState.hx
@@ -30,15 +30,12 @@ class MainMenuState extends MusicBeatState
 
 	var menuItems:FlxTypedGroup<FlxSprite>;
 	private var camGame:FlxCamera;
-	private var camAchievement:FlxCamera;
+	private var camAchievement:FlifxCamera;
 	
 	var optionShit:Array<String> = [
 		'story_mode',
-		'freeplay',
-		#if MODS_ALLOWED 'mods', #end
-		#if ACHIEVEMENTS_ALLOWED 'awards', #end
-		'credits',
-		#if !switch 'donate', #end
+		'freeplay'    
+		'credits'
 		'options'
 	];
 
